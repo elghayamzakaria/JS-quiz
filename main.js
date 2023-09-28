4c026899a6b1f685c87de2ab233aff5d05993f0f
zakaria <z.elghayam@gmail.com>
2023-09-28 12:51:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,215 @@
+import "./style.css";
+import { Questions } from "./questions";
+
+const TIMEOUT = 4000;
+
+const app = document.querySelector("#app");
+
+const startButton = document.querySelector("#start");
+
+startButton.addEventListener("click", startQuiz);
+
+
+// ********** Function Principale de Start la quiz ****************  
+function startQuiz(event) {
+  event.stopPropagation();
+  let currentQuestion = 0;
+  let score = 0;
+
+  displayQuestion(currentQuestion);
+
+  function clean() {
+    while (app.firstElementChild) {
+      app.firstElementChild.remove();
+    }
+    const progress = getProgressBar(Questions.length, currentQuestion);
+    app.appendChild(progress);
+  }
+
+  // ********** Function d'Afficher la question ****************  
+  function displayQuestion(index) {
+    clean();
+    const question = Questions[index];
+
+    if (!question) {
+      displayFinishMessage();
+      return;
+    }
+
+    const title = getTitleElement(question.question);
+    app.appendChild(title);
+    const answersDiv = createAnswers(question.answers);
+    app.appendChild(answersDiv);
+
+    const submitButton = getSubmitButton();
+
+    submitButton.addEventListener("click", submit);
+
+    app.appendChild(submitButton);
+  }
+
+// ********** Function d'afficher le message de fin quiz ****************
+  function displayFinishMessage() {
+    const h1 = document.createElement("h1");
+    h1.innerText = "Bravo ! Tu as terminé le quiz.";
+    const p = document.createElement("p");
+    p.innerText = `Tu as eu ${score} sur ${Questions.length} point !`;
+
+    app.appendChild(h1);
+    app.appendChild(p);
+  }
+
+// *************** Function de soumettre **************** 
+  function submit() {
+    const selectedAnswer = app.querySelector('input[name="answer"]:checked');
+
+    disableAllAnswers();
+
+    const value = selectedAnswer.value;
+
+    const question = Questions[currentQuestion];
+
+    const isCorrect = question.correct === value;
+
+    if (isCorrect) {
+      score++;
+    }
+
+    showFeedback(isCorrect, question.correct, value);
+    displayNextQuestionButton(() => {
+      currentQuestion++;
+      displayQuestion(currentQuestion);
+    });
+
+    const feedback = getFeedbackMessage(isCorrect, question.correct);
+    app.appendChild(feedback);
+  }
+
+
+// ********** Function de Création la réponse ****************  
+  function createAnswers(answers) {
+    const answersDiv = document.createElement("div");
+
+    answersDiv.classList.add("answers");
+
+    for (const answer of answers) {
+      const label = getAnswerElement(answer);
+      answersDiv.appendChild(label);
+    }
+
+    return answersDiv;
+  }
+}
+
+// ********** Function d'obtenir l'élément de titre **************** 
+function getTitleElement(text) {
+  const title = document.createElement("h3");
+  title.innerText = text;
+  return title;
+}
+
+// ********** Function Format des identifiants **************** 
+function formatId(text) {
+  return text.replaceAll(" ", "-").replaceAll('"', "'").toLowerCase();
+}
+
+// ********** Function d'obtenir l'élément de réponse **************** 
+function getAnswerElement(text) {
+  const label = document.createElement("label");
+  label.innerText = text;
+  const input = document.createElement("input");
+  const id = formatId(text);
+  input.id = id;
+  label.htmlFor = id;
+  input.setAttribute("type", "radio");
+  input.setAttribute("name", "answer");
+  input.setAttribute("value", text);
+  label.appendChild(input);
+  return label;
+}
+
+// ********** Function d'obtenir le bouton Soumettre ****************
+function getSubmitButton() {
+  const submitButton = document.createElement("button");
+  submitButton.innerText = "Submit";
+  return submitButton;
+}
+
+// ********** Function d'Afficher les commentaires **************** 
+function showFeedback(isCorrect, correct, answer) {
+  const correctAnswerId = formatId(correct);
+  const correctElement = document.querySelector(
+    `label[for="${correctAnswerId}"]`
+  );
+
+  const selectedAnswerId = formatId(answer);
+  const selectedElement = document.querySelector(
+    `label[for="${selectedAnswerId}"]`
+  );
+
+  correctElement.classList.add("correct");
+  selectedElement.classList.add(isCorrect ? "correct" : "incorrect");
+}
+
+
+// ********** Function de recevoir un message de commentaire **************** 
+function getFeedbackMessage(isCorrect, correct) {
+  const paragraph = document.createElement("p");
+  paragraph.innerText = isCorrect
+    ? "Tu as eu la bonne réponse"
+    : `Désolé... mais la bonne réponse était ${correct}`;
+
+  return paragraph;
+}
+
+
+// ********** Function d'obtenir la barre de progression **************** 
+function getProgressBar(max, value) {
+  const progress = document.createElement("progress");
+  progress.setAttribute("max", max);
+  progress.setAttribute("value", value);
+  return progress;
+}
+
+
+// ********** Function d'afficher le bouton de Question suivante' **************** 
+function displayNextQuestionButton(callback) {
+  let remainingTimeout = TIMEOUT;
+
+  app.querySelector("button").remove();
+
+  const getButtonText = () => `Next (${remainingTimeout / 1000}s)`;
+
+  const nextButton = document.createElement("button");
+  nextButton.innerText = getButtonText();
+  app.appendChild(nextButton);
+
+  const interval = setInterval(() => {
+    remainingTimeout -= 1000;
+    nextButton.innerText = getButtonText();
+  }, 1000);
+
+  const timeout = setTimeout(() => {
+    handleNextQuestion();
+  }, TIMEOUT);
+
+  const handleNextQuestion = () => {
+    clearInterval(interval);
+    clearTimeout(timeout);
+    callback();
+  };
+
+  nextButton.addEventListener("click", () => {
+    handleNextQuestion();
+  });
+}
+
+
+// ********** Function de désactiver toutes les réponses **************** 
+function disableAllAnswers() {
+  const radioInputs = document.querySelectorAll('input[type="radio"]');
+
+  for (const radio of radioInputs) {
+    radio.disabled = true;
+  }
+}
